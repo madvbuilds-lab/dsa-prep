6982f98255ee68a2d3728fd9799bc05c2f228079
madvbuilds-lab <madvbuilds@gmail.com>
2026-08-24 16:34:35 +0530

    Reversing a DoublyLinkedList.

--- Linked-List/Doubly-Linked-List/doublyLinkedList.java
+++ Linked-List/Doubly-Linked-List/doublyLinkedList.java
@@ -1,67 +1,125 @@
-public class doublyLinkedList {
-    public static Node deleteAtTail(Node head){
-        if(head == null) return null;
-        if(head.next == null) return null;
-        Node temp = head;
-        while(temp != null){
-            temp = temp.next;
+public class DoublyLinkedList {
+    static class Node {
+        int data;
+        Node next;
+        Node back;
+
+       
+        public Node(int data1, Node next1, Node back1) {
+            data = data1;
+            next = next1;
+            back = back1;
         }
-        temp.prev.next =null;
+
+        
+        public Node(int data1) {
+            data = data1;
+            next = null;
+            back = null;
+        }
+    }
+
+    
+    public static Node convertArr2DLL(int[] arr) {
+        Node head = new Node(arr[0]);
+        Node prev = head;
+
+        for (int i = 1; i < arr.length; i++) {
+            Node temp = new Node(arr[i], null, prev);
+            prev.next = temp;
+            prev = temp;
+        }
+
         return head;
     }
-    public static Node insertAtTail(Node head , int k){
-        Node newNode = new Node(k);
-        if(head ==null){
-            return newNode;
-        }
-        Node tail = head;
-        while(tail.next != null){
-            tail = tail.next;
-        }
-        tail.next = newNode;
-        newNode.prev = tail;
-        return head;
-    }
-    public static void print(Node head){
-        while(head != null){
+
+    
+    public static void print(Node head) {
+        while (head != null) {
             System.out.print(head.data + " ");
             head = head.next;
         }
         System.out.println();
     }
-    public static Node convertArr2DLL (int[] arr){
-        Node head = new Node(arr[0]);
-        Node prev = head;
-        for(int i = 1; i< arr.length; i++){
-            Node temp = new Node(arr[i], null,prev);
-            prev.next = temp ;
-            prev = temp ;
-        } 
+
+    
+    public static Node insertAtTail(Node head, int k) {
+        Node newNode = new Node(k);
+
+        if (head == null) {
+            return newNode;
+        }
+
+        Node tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+
+        tail.next = newNode;
+        newNode.back = tail;
         return head;
     }
-    static class Node {
-    int data;
-    Node next;
-    Node prev;
 
-    Node(int data1, Node next1, Node prev1) {
-        data = data1;
-        next = next1;
-        prev = prev1;
+    
+    public static Node deleteAtTail(Node head) {
+        
+        if (head == null || head.next == null) {
+            return null;
+        }
+
+        Node tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+
+        Node newTail = tail.back;
+        newTail.next = null;   
+        tail.back = null;     
+
+        return head;
     }
 
-    Node(int data1) {
-        data = data1;
-        next = null;
-        prev = null;
+    
+    public static Node reverseDLL(Node head) {
+        if (head == null || head.next == null) {
+            return head;  
+        }
+
+        Node current = head;
+        Node temp = null; 
+
+        while (current != null) {
+            temp = current.back;       
+            current.back = current.next; 
+            current.next = temp;        
+            current = current.back;     
+        }
+
+        if (temp != null) {
+            head = temp.back;
+        }
+
+        return head;
     }
-}
+
     public static void main(String[] args) {
-        int[] arr = {2, 5, 8, 7};
+        int[] arr = {12, 5, 8, 7, 4};
 
         Node head = convertArr2DLL(arr);
-        System.out.println(head);
-        head = insertAtTail(head,10);
+
+        System.out.println("Doubly Linked List Initially: ");
+        print(head);
+
+        System.out.println("\nDoubly Linked List After Inserting at the tail with value 10: ");
+        head = insertAtTail(head, 10);
+        print(head);
+
+        System.out.println("\nDoubly Linked List After Deleting the tail: ");
+        head = deleteAtTail(head);
+        print(head);
+
+        System.out.println("\nDoubly Linked List After Reversing: ");
+        head = reverseDLL(head);
         print(head);
     }
 }
