d1e79d2992a2645be7c9052c8f0629fc74fe8e18
madvbuilds-lab <madvbuilds@gmail.com>
2026-08-24 15:39:14 +0530

    Code implementation of insertion at tail of DLL.

--- Linked-List/Doubly-Linked-List/doublyLinkedList.java
+++ Linked-List/Doubly-Linked-List/doublyLinkedList.java
@@ -1,5 +1,35 @@
-
-class Node {
+public class doublyLinkedList {
+    public static Node insertAtTail(Node head , int k){
+        Node newNode = new Node(k);
+        if(head ==null){
+            return newNode;
+        }
+        Node tail = head;
+        while(tail.next != null){
+            tail = tail.next;
+        }
+        tail.next = newNode;
+        newNode.prev = tail;
+        return head;
+    }
+    public static void print(Node head){
+        while(head != null){
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+    public static Node convertArr2DLL (int[] arr){
+        Node head = new Node(arr[0]);
+        Node prev = head;
+        for(int i = 1; i< arr.length; i++){
+            Node temp = new Node(arr[i], null,prev);
+            prev.next = temp ;
+            prev = temp ;
+        } 
+        return head;
+    }
+    static class Node {
     int data;
     Node next;
     Node prev;
@@ -16,13 +46,12 @@ class Node {
         prev = null;
     }
 }
-
-public class doublyLinkedList {
     public static void main(String[] args) {
         int[] arr = {2, 5, 8, 7};
 
-        Node head = new Node(arr[0]);
+        Node head = convertArr2DLL(arr);
         System.out.println(head);
-        System.out.println(head.data);
+        head = insertAtTail(head,10);
+        print(head);
     }
 }
