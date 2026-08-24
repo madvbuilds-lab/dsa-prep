93cc84f49edea86a17362d514eef5c7222921a55
madvbuilds-lab <madvbuilds@gmail.com>
2026-08-24 09:36:07 +0530

    Finding the missing number in an Array.

---
+++ arrays/Easy/Find_missing_num.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Find_missing_num{
+    public static int missNum(int[] arr){
+        long n = arr.length;
+        long sum = 0;
+        for(int i = 0 ; i < n ; i++){
+            sum += arr[i];
+        }
+        long exactSum = n * (n+1) / 2 ;
+        return (int)(exactSum - sum);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of an array : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = sc.nextInt(); 
+        }
+        int res = missNum(arr);
+        System.out.print(res); 
+        sc.close();       
+    }
+}
